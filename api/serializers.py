96aa5e4635ed915d9d61bc92390713e3ea65da4a
Shaxv <shaxvideos@gmail.com>
2022-02-04 22:48:38 +0100

    Register UI and Profile page design partly done..

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from wsgiref import validate
 from rest_framework import serializers, validators
 from .models import Computer, Profile
@@ -11,19 +12,33 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ("id", "username", "email")
 
 class ProfileSerializer(serializers.ModelSerializer):
+    birth_date = serializers.DateField(required=True)
+    country = serializers.CharField(required=True, max_length=50)
+    address = serializers.CharField(required=True, max_length=50)
+    avatar = serializers.ImageField()
+
+    def create(self, validated_data):
+        profile = Profile.objects.get_or_create(user=user)
+        profile.birth_date = validated_data["birth_date"]
+        profile.country = validated_data["country"]
+        profile.address = validated_data["address"]
+        profile.avatar = validated_data["avatar"]
+        profile.save()
+
     class Meta:
         model = Profile
-        fields = ("birth_date", "country", "avatar")
+        fields = ("birth_date", "country", "address" "avatar")
 
 class RegisterSerializer(serializers.ModelSerializer):
     email  = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     username = serializers.CharField(max_length=32, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(min_length=6, max_length=100, write_only=True)
-    
+
     def create(self, validated_data):
         user = User(username=validated_data["username"], email=validated_data["email"])
         user.set_password(validated_data["password"])
         user.save()
+
         return user
 
     class Meta:

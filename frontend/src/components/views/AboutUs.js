4760d2493bf4f9c3396e79967f80565d2d1b159f
Shaxv <shaxvideos@gmail.com>
2022-03-19 11:24:04 +0100

    Contact Us and About Us Page UI

--- frontend/src/components/views/AboutUs.js
+++ frontend/src/components/views/AboutUs.js
@@ -2,6 +2,83 @@ import React, {Component} from "react"
 
 export default function AboutUs(){
     return (
-        <h1>Rólunk</h1>
+        <>
+        <div className="about-section-1 main-wrapper">
+            <h1 className="bg-text about-bg-text">CONTACT US</h1>
+            <h1 className="title about-title">Rólunk</h1>
+
+            <div className="about-featured">
+                <div className="about-card">
+                    <h2 className="about-card-title">Megbízhatóság szerte a világon</h2>
+                    <div className="row">
+                        <div className="col-12 col-md-4">
+                            <div className="about-featured-text">
+                                <h2>{">"} 30,000</h2>
+                                <span>Ügyfelek</span>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4">
+                            <div className="about-featured-text not-first">
+                                <h2>12+</h2>
+                                <span>Fizikai bolt</span>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4">
+                            <div className="about-featured-text not-first">
+                                <h2>100%</h2>
+                                <span>Garancia</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr className="about-hr"/>
+
+        <div className="about-people">
+            <div className="people-card">
+                <div className="d-flex">
+                <div className="symbol">
+                    <h1>BG</h1>
+                </div>
+                <div className="main-area">
+                    <h1>Bill Gates</h1>
+                    <h5 className="sup-title">Társ alapító, Vezérigazgató</h5>
+                    <hr className="main-area-hr"/>
+                    <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</span>
+                    <div className="d-flex brands">
+                        <img src="/static/images/svg/facebook.svg" height={16}/>
+                        <img src="/static/images/svg/instagram.svg" height={16}/>
+                        <img src="/static/images/svg/twitter.svg" height={16}/>
+                        <img src="/static/images/svg/youtube.svg" height={16}/>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="about-people mt-5 mb-5">
+            <div className="people-card">
+                <div className="d-flex">
+                <div className="symbol">
+                    <h1>MZ</h1>
+                </div>
+                <div className="main-area">
+                    <h1>Mark Zuckerberg</h1>
+                    <h5 className="sup-title">Társ alapító, Műszaki vezérigazgató</h5>
+                    <hr className="main-area-hr"/>
+                    <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</span>
+                    <div className="d-flex brands">
+                        <img src="/static/images/svg/facebook.svg" height={16}/>
+                        <img src="/static/images/svg/instagram.svg" height={16}/>
+                        <img src="/static/images/svg/twitter.svg" height={16}/>
+                        <img src="/static/images/svg/youtube.svg" height={16}/>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </>
     )
 }
--- frontend/src/components/views/ContactUs.js
+++ frontend/src/components/views/ContactUs.js
@@ -3,7 +3,49 @@ import React, {Component} from "react"
 export default function ContactUs(){
     return (
         <>
-            <h1>Contact Us</h1>
-      </>
+        <div className="contact-section-1 main-wrapper">
+            <h1 className="bg-text contact-bg-text">SUPPORT</h1>
+            <h1 className="title contact-title">Elérhetőség</h1>
+
+            <div className="contact-featured">
+                <div className="contact-card">
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <h1>Értékesítés</h1>
+                            <hr/>
+                            <div className="d-flex">
+                                <img src="/static/images/svg/phone.svg" height={16}/>
+                                <span>+36 20 999 9999</span>
+                            </div>
+                            <div className="d-flex">
+                                <img src="/static/images/svg/chat.svg" height={16}/>
+                                <span>support@hardwared.com</span>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <h1>Szervízelés</h1>
+                            <hr/>
+                            <div className="d-flex">
+                                <img src="/static/images/svg/phone.svg" height={16}/>
+                                <span>+36 20 999 9998</span>
+                            </div>
+                            <div className="d-flex">
+                                <img src="/static/images/svg/chat.svg" height={16}/>
+                                <span>support@hardwared.com</span>
+                            </div>
+                        </div>
+                    </div>
+                    <h2>Bécsi út 277.</h2>
+                    <div style={{width: "100%"}}><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=Budapest,%20B%C3%A9csi%20%C3%BAt%20277,%201037+(Hardwared)&amp;t=h&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/marine-gps/"></a></iframe></div>
+
+                    <h2>Nyugati tér 4.</h2>
+                    <div style={{width: "100%"}}><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=Budapest,%20Nyugati%20t%C3%A9r%204,%201132+(Hardwared)&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/marine-gps/"></a></iframe></div>
+                </div>
+            </div>
+        </div>
+
+        <hr className="contact-hr"/>
+
+    </>
     )
 }
